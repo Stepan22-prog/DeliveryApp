568457ea49c7d40db4728589db70fcdac87f21b9
Stepan Stadniuk <stepanigcom@gmail.com>
2024-03-09 15:09:01 +0200

    +header component

---
+++ elifTechStore/src/shared/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="header">
+        <nav className="header-nav">
+            <Link to="/" className="header-link">Shop</Link>
+            <Link to="/cart" className="header-link">Shopping Cart</Link>
+        </nav>
+    </header>
+  )
+}
