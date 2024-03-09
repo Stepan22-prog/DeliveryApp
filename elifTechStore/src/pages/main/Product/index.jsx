0cac6cbed2f47971f7af617372d9a194f5fc04dc
Stepan Stadniuk <stepanigcom@gmail.com>
2024-03-09 16:00:27 +0200

    + list of products

---
+++ elifTechStore/src/pages/main/Product/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function Product({title, img, price, setToCart}) {
+  return (
+    <div className="product">
+        <div className="product__img">
+            <img src={img} alt={title} />
+        </div>
+        <h2 className="product__title">{title}</h2>
+        <div className="product__row">
+            <h3 className="product__price">{price}</h3>
+            <button onClick={() => setToCart()} className="product__btn">Add to Cart</button>
+        </div>
+    </div>
+  )
+}
--- elifTechStore/src/pages/main/index.jsx
+++ elifTechStore/src/pages/main/index.jsx
@@ -1,5 +1,15 @@
-export default function Main() {
-  return (
-    <div>index</div>
-  )
+import { useState } from "react";
+import Product from "./Product";
+
+export default function Main({ setToCart }) {
+  const [products, setProducts] = useState([]);
+  return products.map((product) => {
+		<Product 
+			key={product.id} 
+			title={product.title} 
+			img={product.img}
+			price={product.price}
+			setToCart={() => setToCart(product.id)}
+		/>
+	})
 }
