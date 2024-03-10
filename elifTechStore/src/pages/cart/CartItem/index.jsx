aa2f428725d1c0dd2370312c0b2ceda0a356e39d
Stepan Stadniuk <stepanigcom@gmail.com>
2024-03-10 15:46:35 +0200

    + cart item

---
+++ elifTechStore/src/pages/cart/CartItem/index.jsx
@@ -0,0 +1,37 @@
+import Input from "../../../shared/Input"
+
+export default function CartItem({ id, title, photo, count, price, setToCart }) {
+  function updatePrice(count) {
+    console.log({count});
+    setToCart((prevValue) => {
+      return prevValue.map(
+        (cartItem) => cartItem.id === id ? {...cartItem, count} : cartItem
+      )
+    })
+  }
+  
+  return (
+    <div className="cart-item">
+      <div className="cart-item__img">
+        <img src={photo} alt={title} />
+      </div>
+      <div className="cart-item-info">
+        <h2 className="cart-item-info__title">{title}</h2>
+        <div className="cart-item-count">
+          <button className="cart-item-count__btn" onClick={() => updatePrice(count + 1)}>+</button>
+          <Input
+            id="count"
+            type="number"
+            value={count}
+            setValue={(value) => updatePrice(value)}
+          />
+          <button className="cart-item-count__btn" onClick={() => updatePrice(count - 1)}>-</button>
+        </div>
+        <div className="cart-item-info__row">
+          <h3 className="cart-item-info__price">{count * price}</h3>
+          <button className="cart-item-info__delete">delete</button>
+        </div>
+      </div>
+    </div>
+  )
+}
